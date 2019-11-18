7a8ec7e5d8b95cc0e62f9467dcc06ab8774c7e3d
hyeonahkiki <kkongc7@gmail.com>
2019-11-18 16:29:37 +0900

    jwt token

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,21 +1,56 @@
 <template>
-  <div class="login-div col-6 offset-3">
-    <div class="form-group">
-      <!-- for는 뭐랑 연결시킬지 -->
-      <label for="id">ID</label>
-      <input id="id" class="form-control" type="text">
+  <div>
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
     </div>
-    <div class="form-group">
-      <label for="password">PW</label>
-      <input id="password" class="form-control" type="password">
+    <div v-else class="login-div col-6 offset-3">
+      <div class="form-group">
+        <!-- for는 뭐랑 연결시킬지 -->
+        <label for="id">ID</label>
+        <input id="id" 
+        class="form-control" 
+        type="text" 
+        v-model="credential.username">
+      </div>
+      <div class="form-group">
+        <label for="password">PW</label>
+        <input id="password" 
+        class="form-control" 
+        type="password"
+        v-model="credential.password">
+      </div>
+      <button class="btn btn-primary" @click="login">로그인</button>
     </div>
-    <button class="btn btn-primary">로그인</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  // 데이터는 다른곳에서 접근이 안되고 LoginForm안에서만 접근가능
+  data: function(){
+    return {
+      credential: {
+        username: '',
+        password: '',
+      },
+      loading: false,
+    }
+  },
+  methods: {
+    login(){
+      console.log('로그인시도!!')
+      axios.post('http://localhost:8000/api-token-auth/', this.credential)
+      .then((res)=>{
+        this.loading = true
+        console.log(res)
+      })
+      .catch((e)=>{
+        this.loading = true
+        console.log(e)
+      })
+    }
+  }
 }
 </script>
 
